5873a0c6342bf4e47cfc712e765459b3bef3603b
mohsensami <mohsensami88@gmail.com>
2024-01-05 10:28:02 +0330

    show restaurant point in the google map

--- client/screens/DeliveryScreen.js
+++ client/screens/DeliveryScreen.js
@@ -1,10 +1,31 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
+import { featured } from '../constants';
 
 export default function DeliveryScreen() {
+    const resturant = featured.restaurants[0];
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text>DeliveryScreen</Text>
+        <View className="flex-1">
+            <MapView
+                initialRegion={{
+                    latitude: resturant.lat,
+                    longitude: resturant.lng,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+                className="flex-1"
+                mapType="standard"
+            >
+                <Marker
+                    coordinate={{
+                        latitude: resturant.lat,
+                        longitude: resturant.lng,
+                    }}
+                />
+            </MapView>
         </View>
     );
 }
